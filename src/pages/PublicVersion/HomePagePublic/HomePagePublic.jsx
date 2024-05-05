14f424fbfa73fc9a7a601b7402a3d642b5aacbd9
GlebShylovich <glebshylovich10@gmail.com>
2024-05-05 10:49:04 +0200

    Progress bar remade

--- src/pages/PublicVersion/HomePagePublic/HomePagePublic.jsx
+++ src/pages/PublicVersion/HomePagePublic/HomePagePublic.jsx
@@ -3,7 +3,7 @@ import MirrorCode from "../../../components/MirrorCode/MirrorCode.jsx";
 import ResultCode from "../../../components/ResultCode/ResultCode.jsx";
 import validateTask from "../../../validateTask/allTasksValidate.js";
 import ProgressBar from "../../../components/ProgressBar/ProgressBar.jsx"
-import {useAuth} from "../../../hooks/use-auth.js";
+import { useAuth } from "../../../hooks/use-auth.js";
 import tasksPublic from "../tasksPublic.json";
 import "./HomePagePublic.scss";
 import { Modal } from "antd";
@@ -17,33 +17,33 @@ import arrowHeart from "../../../assets/images/homePage/arrow-heart.png";
 import bulb from "../../../assets/images/homePage/bulb.png";
 
 export default function HomePagePublic({ setDisabledFooter }) {
-  const [value, setValue] = useState("");
-  const [numberTask, setNumberTask] = useState(0);
-  const [openModal, setOpenModal] = useState(true);
-  const [falseValidate, setFalseValidate] = useState(false);
-  const [trueValidate, setTrueValidate] = useState(false);
-  const { id, token } = useAuth();
+    const [value, setValue] = useState("");
+    const [numberTask, setNumberTask] = useState(0);
+    const [openModal, setOpenModal] = useState(true);
+    const [falseValidate, setFalseValidate] = useState(false);
+    const [trueValidate, setTrueValidate] = useState(false);
+    const { id, token } = useAuth();
 
-  useEffect(() => {
-    setValue(tasksPublic[numberTask].valueRedactor);
-  }, [numberTask]);
+    useEffect(() => {
+        setValue(tasksPublic[numberTask].valueRedactor);
+    }, [numberTask]);
 
-  function getTaskUser() {
-    setOpenModal(false);
-  }
+    function getTaskUser() {
+        setOpenModal(false);
+    }
 
-  function sendValidate() {
-    const result = validateTask(value, `task${numberTask + 1}`);
-    if (!result) setFalseValidate(true);
-    if (result) setTrueValidate(true);
+    function sendValidate() {
+        const result = validateTask(value, `task${numberTask + 1}`);
+        if (!result) setFalseValidate(true);
+        if (result) setTrueValidate(true);
 
-    console.log(token);
-  }
+        console.log(token);
+    }
 
-  function nextTask() {
-    setTrueValidate(false);
-    setNumberTask((prevState) => prevState + 1);
-  }
+    function nextTask() {
+        setTrueValidate(false);
+        setNumberTask((prevState) => prevState + 1);
+    }
 
     return (
         <>
@@ -58,17 +58,17 @@ export default function HomePagePublic({ setDisabledFooter }) {
 
                     <div className="homePublicPage__modal-container">
                         <h2 className="homePublicPage__modal-title">{tasksPublic[numberTask].title} <img
-                            src={stripesModal} alt="stripes modal" className="homePublicPage__modal-stripes"/></h2>
+                            src={stripesModal} alt="stripes modal" className="homePublicPage__modal-stripes" /></h2>
                         <p className="homePublicPage__modal-text">{tasksPublic[numberTask].theory}</p>
-                        <img src={arrowModal} alt="arrow modal" className="homePublicPage__modal-arrow"/>
+                        <img src={arrowModal} alt="arrow modal" className="homePublicPage__modal-arrow" />
                         <button onClick={() => getTaskUser()} className="homePublicPage__modal-btn">
                             Мне все понятно! Показать задание
                         </button>
                     </div>
                 </Modal>
-                <ProgressBar allTasks={15} currentTask={4}/>
                 <div className='homePublicPage__left'>
                     <div className="homePublicPage__taskPublic">
+                        <ProgressBar allTasks={tasksPublic.length} currentTask={1} />
                         <div className="homePublicPage__block">
                             <div className="homePublicPage__exercise">
                                 <h2 className="homePublicPage__exercise-title">
@@ -102,12 +102,12 @@ export default function HomePagePublic({ setDisabledFooter }) {
                         </div>
                         <div className="homePublicPage__result">
                             <div className="homePublicPage__result-images">
-                                <img src={catHomePage} alt="cat" className="homePublicPage__result-cat"/>
-                                <img src={arrowHeart} alt="arrow heart" className="homePublicPage__result-arrow"/>
+                                <img src={catHomePage} alt="cat" className="homePublicPage__result-cat" />
+                                <img src={arrowHeart} alt="arrow heart" className="homePublicPage__result-arrow" />
                             </div>
                             <div className="homePublicPage__result-block">
                                 <div className="homePublicPage__result-text">
-                                    <ResultCode value={value}/>
+                                    <ResultCode value={value} />
                                 </div>
                             </div>
                         </div>
@@ -115,7 +115,7 @@ export default function HomePagePublic({ setDisabledFooter }) {
                 </div>
                 <div className={`homePublicPage__right ${trueValidate ? 'right_true' : null} `}>
                     <div className={!falseValidate ? 'homePublicPage__answer' : 'homePublicPage__answer errorBorder'}>
-                        <MirrorCode value={value} setValue={setValue} setFalseValidate={setFalseValidate}/>
+                        <MirrorCode value={value} setValue={setValue} setFalseValidate={setFalseValidate} />
                         <div className="homePublicPage__hint">
                             <button className="homePublicPage__hint-btn">
                                 <img
@@ -128,7 +128,7 @@ export default function HomePagePublic({ setDisabledFooter }) {
                         </div>
                         {trueValidate && <div className='box_sucsessfully'>
                             <div className='img_sucsessfully'>
-                                <img src={catSucs} alt='cat'/>
+                                <img src={catSucs} alt='cat' />
                             </div>
                             <h3>Ура! У тебя получлилось, мы можем идти дальше</h3>
                         </div>}
