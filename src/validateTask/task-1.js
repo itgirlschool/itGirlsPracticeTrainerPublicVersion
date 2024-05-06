5fc765b5f26b3803698a545b038dea15b2ed5e27
Daria Demidova <daashenkaa@mail.ru>
2024-05-06 23:11:25 +0300

    add tasks validation

--- src/validateTask/task-1.js
+++ src/validateTask/task-1.js
@@ -1,3 +1,17 @@
-export default function  task1 (value){
-    return true
+// import tasksPublic from "../pages/PublicVersion/tasksPublic.json";
+
+export default function task1(value) {
+    
+    // const correctAnswer = tasksPublic[0].correctAnswer;
+    const correctAnswer = '<!DOCTYPE html><html lang="ru"><head><title>Я заголовок страницы</title></head><body>Я содержимое страницы</body> </html>';
+    console.log(correctAnswer);
+    console.log(value);
+
+    if (value.trim() !== correctAnswer.trim()) {
+        return false;
+    } else {
+        return true;
+    } 
+
 }
+
