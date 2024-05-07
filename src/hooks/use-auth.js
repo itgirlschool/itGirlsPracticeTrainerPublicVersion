1396ae89d073843c74a090a09e0aeb29f30196f9
GlebShylovich <glebshylovich10@gmail.com>
2024-05-07 12:01:55 +0200

    answer modal

--- src/hooks/use-auth.js
+++ src/hooks/use-auth.js
@@ -3,6 +3,5 @@ import {useSelector} from "react-redux";
 
 export  function  useAuth(){
     const {email,id,displayName,statusUser,phone,date,password} = useSelector(state => state.user)
-    return {useAuthBool:!!email,
-    email,id,displayName,phone,date,statusUser,password}
+    return {useAuthBool:!!email, email,id,displayName,phone,date,statusUser,password}
 }
--- src/pages/PublicVersion/HomePagePublic/HomePagePublic.jsx
+++ src/pages/PublicVersion/HomePagePublic/HomePagePublic.jsx
@@ -1,13 +1,14 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import MirrorCode from "../../../components/MirrorCode/MirrorCode.jsx";
 import ResultCode from "../../../components/ResultCode/ResultCode.jsx";
 import validateTask from "../../../validateTask/allTasksValidate.js";
-import ProgressBar from "../../../components/ProgressBar/ProgressBar.jsx"
-import {useEditData} from "../../../Services/Firebade_realTime/services.js";
-import {useAuth} from "../../../hooks/use-auth.js";
+import ProgressBar from "../../../components/ProgressBar/ProgressBar.jsx";
+import ModalAnswer from "../../../components/ModalAnswer/ModalAnswer.jsx";
+import { useEditData } from "../../../Services/Firebade_realTime/services.js";
+import { useAuth } from "../../../hooks/use-auth.js";
 import tasksPublic from "../tasksPublic.json";
 import "./HomePagePublic.scss";
-import {Modal} from "antd";
+import { Modal } from "antd";
 
 import arrowModal from "../../../assets/images/homePage/arrow-modal.png";
 import stripesModal from "../../../assets/images/homePage/stripes-modal.png";
@@ -16,16 +17,17 @@ import rhombus from "../../../assets/images/homePage/rhombus.png";
 import catHomePage from "../../../assets/images/homePage/cat-home-page.png";
 import arrowHeart from "../../../assets/images/homePage/arrow-heart.png";
 import bulb from "../../../assets/images/homePage/bulb.png";
-import {log} from "loglevel";
+import { log } from "loglevel";
 
-export default function HomePagePublic({setDisabledFooter}) {
+export default function HomePagePublic({ setDisabledFooter }) {
     const [value, setValue] = useState("");
     const [numberTask, setNumberTask] = useState(0);
     const [openModal, setOpenModal] = useState(true);
     const [validate, setValidate] = useState('default');
-    const {email, id, displayName, phone, date, statusUser, password} = useAuth();
+    const { email, id, displayName, phone, date, statusUser, password } = useAuth();
     const mutateEdit = useEditData();
     const [showResultImages, setShowResultImages] = useState(true);
+    const [openAnswerModal, setOpenAnswerModal] = useState(false);
 
     useEffect(() => {
         setValue(tasksPublic[numberTask].valueRedactor);
@@ -34,6 +36,10 @@ export default function HomePagePublic({setDisabledFooter}) {
     function getTaskUser() {
         setOpenModal(false);
     }
+
+    useEffect(() => {
+        console.log(validate);
+    }, [sendValidate])
 
     function sendValidate() {
         const result = validateTask(value, `task${numberTask + 1}`)
@@ -68,9 +74,9 @@ export default function HomePagePublic({setDisabledFooter}) {
 
                     <div className="homePublicPage__modal-container">
                         <h2 className="homePublicPage__modal-title">{tasksPublic[numberTask].title} <img
-                            src={stripesModal} alt="stripes modal" className="homePublicPage__modal-stripes"/></h2>
+                            src={stripesModal} alt="stripes modal" className="homePublicPage__modal-stripes" /></h2>
                         <p className="homePublicPage__modal-text">{tasksPublic[numberTask].theory}</p>
-                        <img src={arrowModal} alt="arrow modal" className="homePublicPage__modal-arrow"/>
+                        <img src={arrowModal} alt="arrow modal" className="homePublicPage__modal-arrow" />
                         <button onClick={() => getTaskUser()} className="homePublicPage__modal-btn">
                             Мне все понятно! Показать задание
                         </button>
@@ -78,7 +84,7 @@ export default function HomePagePublic({setDisabledFooter}) {
                 </Modal>
                 <div className='homePublicPage__left'>
                     <div className="homePublicPage__taskPublic">
-                        <ProgressBar allTasks={tasksPublic.length} currentTask={numberTask}/>
+                        <ProgressBar allTasks={tasksPublic.length} currentTask={numberTask} />
                         <div className="homePublicPage__block">
                             <div className="homePublicPage__exercise">
                                 <h2 className="homePublicPage__exercise-title">
@@ -112,15 +118,15 @@ export default function HomePagePublic({setDisabledFooter}) {
                         </div>
                         <div className="homePublicPage__result">
                             {showResultImages ? (
-                                    <div className="homePublicPage__result-images">
-                                        <img src={catHomePage} alt="cat" className="homePublicPage__result-cat"/>
-                                        <img src={arrowHeart} alt="arrow heart" className="homePublicPage__result-arrow"/>
-                                        <h2>Приступай к выполнению задания и ты увидишь тут результат</h2>
-                                    </div>
-                                ) :
+                                <div className="homePublicPage__result-images">
+                                    <img src={catHomePage} alt="cat" className="homePublicPage__result-cat" />
+                                    <img src={arrowHeart} alt="arrow heart" className="homePublicPage__result-arrow" />
+                                    <h2>Приступай к выполнению задания и ты увидишь тут результат</h2>
+                                </div>
+                            ) :
                                 <div className="homePublicPage__result-block">
                                     <div className="homePublicPage__result-text">
-                                        <ResultCode value={value}/>
+                                        <ResultCode value={value} />
                                     </div>
                                 </div>
                             }
@@ -130,7 +136,7 @@ export default function HomePagePublic({setDisabledFooter}) {
                 <div className='homePublicPage__right'>
                     <div className='homePublicPage__answer'>
                         <MirrorCode setShowResultImages={setShowResultImages} value={value} setValue={setValue}
-                                    setValidate={validate}/>
+                            setValidate={validate} />
                         <div className="homePublicPage__hint">
                             <button className="homePublicPage__hint-btn">
                                 <img
@@ -144,13 +150,13 @@ export default function HomePagePublic({setDisabledFooter}) {
                     </div>
                     <div className="homePublicPage__check">
                         {validate === 'default' || validate === 'error' ?
-                            <button className="homePublicPage__check-btn" onClick={sendValidate}>Проверить</button> :
+                            <button className="homePublicPage__check-btn" onClick={() => { sendValidate(); setOpenAnswerModal(true); }}>Проверить</button> :
                             <button className="homePublicPage__check-btn next_task" onClick={nextTask}>Следующая
                                 задача</button>
                         }
-
                     </div>
                 </div>
+                {openAnswerModal && (<ModalAnswer setOpenAnswerModal={setOpenAnswerModal} validate={validate} openAnswerModal={openAnswerModal}/>)}
             </div>
         </>
     );
