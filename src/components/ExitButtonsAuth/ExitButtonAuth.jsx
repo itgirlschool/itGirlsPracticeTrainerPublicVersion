8eaa161951221f1050b03759cab50023b46b5fe8
Ekaterina Antsiferova <itkatprogrammer@gmail.com>
2024-05-08 18:08:42 +0400

    fix layout & add btn log out for mobile version too

--- src/components/ExitButtonsAuth/ExitButtonAuth.jsx
+++ src/components/ExitButtonsAuth/ExitButtonAuth.jsx
@@ -1,26 +1,26 @@
 import { getAuth, signOut } from 'firebase/auth';
-import {removeUser} from "../../store/slices/userSlices.js";
-import {useDispatch} from "react-redux";
-import React from "react";
-
+import { removeUser } from '../../store/slices/userSlices.js';
+import { useDispatch } from 'react-redux';
+import React from 'react';
+import './ExitButtonAuth.scss';
 
 const ExitButton = React.memo(() => {
-const dispatch = useDispatch()
-    const handleSignOut = async () => {
-        const auth = getAuth();
-        try {
-            await signOut(auth).then(()=>{
-                dispatch(removeUser());
-            })
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  const dispatch = useDispatch();
+  const handleSignOut = async () => {
+    const auth = getAuth();
+    try {
+      await signOut(auth).then(() => {
+        dispatch(removeUser());
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-        <button className="exitBtnStudy" onClick={handleSignOut}>
-            Выйти из личного кабинета
-        </button>
-    );
+  return (
+    <button className='exitBtnStudy' onClick={handleSignOut}>
+      Выйти из личного кабинета
+    </button>
+  );
 });
-export  default ExitButton
+export default ExitButton;
