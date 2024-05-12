993349f9a9385c8aaf881e35a407b19c7e0176b3
Egor9000662 <egor1908608@gmail.com>
2024-05-12 11:49:41 +0300

    add valid task 1

--- src/validateTask/task-1.js
+++ src/validateTask/task-1.js
@@ -1,18 +1,12 @@
 export default function task1(value) {
 
-//     const correctAnswer = `
-//     <!DOCTYPE html>
-// <html lang="ru">
-// <head>
-//   <title>Я заголовок страницы</title>
-// </head>
-// <body>Я содержимое страницы</body>
-// </html>`
-
-
-//     if (value.trim() !== correctAnswer.trim()) {
-//         return false;
-//     } else {
-        return true;
-    }
-// }
+     const correctAnswer = `<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <title>Я заголовок страницы</title>
+</head>
+<body>Я содержимое страницы</body>
+</html>
+     `
+    return value.trim() === correctAnswer.trim();
+ }
